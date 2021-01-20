444ccaab99e5e9f542f5acfaab9b25882de6b81a
rajat-gajera <rajatgajera2212@gmail.com>
2021-01-20 19:50:33 +0530

    Initial Commit

--- app/src/main/java/com/example/keepintouch/Groups.java
+++ app/src/main/java/com/example/keepintouch/Groups.java
@@ -1,5 +1,6 @@
 package com.example.keepintouch;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,24 +10,29 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 import java.security.acl.Group;
 import java.util.ArrayList;
 
-public class Groups extends Fragment {
+public class Groups extends Fragment implements DialogBox.DialogBoxListener {
+    private static final String GROUPNAME ="",ADMINNAME="" ;
     private ArrayList<GroupItem> mGroupList;
     private  TextView mGroupName,mAdminName;
     private RecyclerView  mRecyclerView;
     private GroupAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private FloatingActionButton fab;
+    //private FloatingActionButton fab;
+    private FloatingActionButton mJoinButton,mCreateGroupButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -40,7 +46,21 @@ public class Groups extends Fragment {
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        fab = rootview.findViewById(R.id.floating_btn);
+         mJoinButton = rootview.findViewById(R.id.join_button);
+        mCreateGroupButton = rootview.findViewById(R.id.create_button);
+        mJoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mCreateGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenDialog();
+            }
+        });
 
 
 
@@ -52,15 +72,13 @@ public class Groups extends Fragment {
             }
         });
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-
-
-        });
         return  rootview;
+    }
+
+    private void OpenDialog() {
+        DialogBox dialogBox = new DialogBox();
+        dialogBox.show(getFragmentManager(),"Create Group");
     }
 
     private void createGroupList() {
@@ -71,5 +89,15 @@ public class Groups extends Fragment {
     }
 
 
+    @SuppressLint("ResourceType")
+    @Override
+    public void createGroup(AppCompatDialogFragment dialogFragment) {
 
+       String groupname,adminname;
+       groupname=dialogFragment.getString(R.id.group_name);
+       adminname = dialogFragment.getString(R.id.admin_name);
+
+       mGroupList.add(new GroupItem(groupname,adminname));
+
+    }
 }
